f4d52cb147d0bfcf39e4f8727914df2055725604
Jakob Huber <93046@psdschools.org>
2022-08-22 09:52:12 -0600

    Initial Commit

---
+++ UIImageView/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swif
+//  UIImageView
+//
+//  Created by Huber, Jakob - Student on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    //Declare Outlets and Variables
+    
+    @IBOutlet var myimage: UIImageView!
+    
+    let myimage = UIImage(named: "AccentColor")
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+    
+    
+            
+            
+            
+        
+}
